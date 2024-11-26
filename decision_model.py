0399361cf456c92342402404dc0967dfca75c0f3
Christopher Strickland <cstric12@utk.edu>
2024-11-26 11:11:41 -0500

    Adds segment geometry

--- decision_model.py
+++ decision_model.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class targets:
 
-    def __init__(self, pos=None, theta=None, geom=None):
+    def __init__(self, pos=None, geom_name=None, r=None, l=None, theta=None):
         '''Set up targets for attraction model.
         The only thing taken care of here is storage of target locations and 
         calculation of unbiased, unwarped perception of the targets (angluar 
@@ -21,10 +21,23 @@ class targets:
         ----------
         pos : Nx2 ndarray (default=np.array([[15,5],[15,15]]))
             x,y coordinates of targets
-        theta : length N ndarray TODO
-            orientation of targets
-        geom : {TODO}, (optional)
-            geometry of targets
+        geom : {'circle'}, (optional)
+            geometry of targets. Depending on the choice, additional parameters 
+            must be set to quantify the geometry. Options are:
+            - 'circle' : must specify a radius r to be used for all targets or 
+            an array of radii r, one for each target. The position of the target 
+            is the midpoint of the circle.
+            - 'segment' : must specify a length l to be used for all targets or 
+            an array of lengths l, one for each target. Similarly, must specify 
+            a theta (for all targets or each), specifying the angle each target 
+            segment is at in the plane. The position of the target is the 
+            midpoint of the segment.
+        r : float or ndarray
+            radius of circles in the geometry; see geom for requirements
+        l : float or ndarray
+            line segment lengths in the geometry; see geom for requirements
+        theta : float or length N ndarray
+            orientation of targets; see geom for requirements
         '''
 
         if pos is None:
@@ -32,7 +45,10 @@ class targets:
         else:
             self.locs = pos
         self.angles = theta
-        self.geom = geom
+        self.geom_name = geom_name
+        self.r = r
+        self.l = l
+        self.theta = theta
 
     
     def get_percep_angles(self,loc,angle=0):
@@ -51,11 +67,38 @@ class targets:
         length N ndarray of single theta values instead.
         '''
 
-        # Get a vector toward each target
-        vecs = self.locs - loc
-
-        if self.geom is None:
+        if self.geom_name is None:
             ##### Point targets #####
+            # Get a vector toward each target
+            vecs = self.locs - loc
             target_angles = np.arctan2(vecs[:,1],vecs[:,0])
             return target_angles - angle
+        elif self.geom_name == 'circle':
+            vecs = self.locs - loc
+            target_angles = np.arctan2(vecs[:,1],vecs[:,0])
+            if vecs.ndim > 1:
+                vecs_length = np.linalg.norm(vecs, axis=1)
+            else:
+                vecs_length = np.linalg.norm(vecs)
+            pm_theta = np.arcsin(self.r/vecs_length)
+            return np.column_stack([target_angles-pm_theta,target_angles+pm_theta])
+        elif self.geom_name == 'segment':
+            # find location of segment endpoints
+            diff = np.column_stack([self.l/2*np.cos(self.theta),
+                                    self.l/2*np.sin(self.theta)])
+            endpt1 = self.locs + diff
+            endpt2 = self.locs - diff # difference in heading angle is pi between seg endpoints
+            # get a vector to each
+            vecs1 = endpt1 - loc; vecs2 = endpt2 - loc
+            # get angles to each
+            angles1 = np.arctan2(vecs1[:,1],vecs1[:,0])
+            angles2 = np.arctan2(vecs2[:,1],vecs2[:,0])
+            # store sorted and return
+            target_angles = np.zeros((len(angles1,2)))
+            one_two = angles1 <= angles2
+            target_angles[one_two,:] = np.column_stack([angles1[one_two],
+                                                        angles2[one_two]])
+            target_angles[~one_two,:] = np.column_stack([angles2[~one_two],
+                                                         angles1[~one_two]])
+            return target_angles
             
