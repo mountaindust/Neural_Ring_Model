0bd24e828b2d1c04c9aa5be9c3591e39b065427d
Christopher Strickland <cstric12@utk.edu>
2024-11-28 12:08:28 -0600

    Demos occlusion case

--- plot_targets.py
+++ plot_targets.py
@@ -9,7 +9,8 @@ import decision_model as model
 
 focal_loc = np.array([5,10])
 
-targets = model.Targets(geom_name='segment', l=1, theta=np.array([0.2,-0.5]))
+targets = model.Targets(pos=np.array([[9,12],[15,14],[13,7]]), geom_name='circle', 
+                        r=np.array([0.5, 1.25, 0.75]))
 focal_angle = -1
 angles = targets.get_percep_angles(focal_loc, focal_angle)
 
